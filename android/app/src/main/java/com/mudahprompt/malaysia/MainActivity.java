d9e93e32bd27b87678b54bf7020f6551c0a5db83
Aaqil Ahamad <72489957+Aaqil456@users.noreply.github.com>
2025-12-26 20:02:25 +0800

    added kotlin and jdk for building web app in a wrapper

---
+++ android/app/src/main/java/com/mudahprompt/malaysia/MainActivity.java
@@ -0,0 +1,5 @@
+package com.mudahprompt.malaysia;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.mudahprompt.malaysia',
+  appName: 'MudahPrompt Malaysia',
+  webDir: 'dist'
+};
+
+export default config;
